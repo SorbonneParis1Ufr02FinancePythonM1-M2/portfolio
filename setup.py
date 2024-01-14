7459e7f0d6556bd499b668e22bde019d2abaacfc
fabgal-p1 <galan.fabrice@gmail.com>
2024-01-14 19:35:43 +0100

    - create setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="main.py",
+    version="1.0",
+    packages=[""],
+    url="https://github.com/p1PythonM1-M2/portfolio",
+    license="",
+    author="fabrice_galan",
+    author_email="",
+    description="code example",
+    install_requires=["matplotlib", "numpy", "pandas"],
+    include_package_data=True,
+)
